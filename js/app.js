c7fd7dc663550b73b01a3ef8467bd789b4bb6751
softvision-raul-bucata <75027050+softvision-raul-bucata@users.noreply.github.com>
2026-04-05 20:54:53 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,3 +17,39 @@ document.addEventListener("click", (e) => {
     menu.classList.remove("active");
   }
 });
+
+// create lightbox
+const lightbox = document.createElement("div");
+lightbox.style.position = "fixed";
+lightbox.style.top = "0";
+lightbox.style.left = "0";
+lightbox.style.width = "100%";
+lightbox.style.height = "100%";
+lightbox.style.background = "rgba(0,0,0,0.9)";
+lightbox.style.display = "none";
+lightbox.style.alignItems = "center";
+lightbox.style.justifyContent = "center";
+lightbox.style.zIndex = "9999";
+document.body.appendChild(lightbox);
+
+const img = document.createElement("img");
+img.style.maxWidth = "90%";
+img.style.maxHeight = "90%";
+img.style.borderRadius = "12px";
+lightbox.appendChild(img);
+
+  // click any image
+images.forEach(image => {
+  image.style.cursor = "pointer";
+
+  image.addEventListener("click", () => {
+    img.src = image.src;
+    lightbox.style.display = "flex";
+    });
+  });
+
+  // close on click
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+  });
+});
